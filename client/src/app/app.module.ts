202a803338cdfcdada4802d3a5f36579c7d4fab0
Jason Pettrone <jasonpettrone@gmail.com>
2021-09-28 00:29:48 -0400

    Added new bootstrap theme.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -35,7 +35,7 @@ import { ToastrModule } from 'ngx-toastr';
     FormsModule,
     BsDropdownModule.forRoot(),
     ToastrModule.forRoot({
-      positionClass: 'toast-bottom-right'
+      positionClass: 'toast-bottom-left'
     })
   ],
   providers: [],
