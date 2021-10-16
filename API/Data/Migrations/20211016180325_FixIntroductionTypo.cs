cbd6d969a60a28e40a0357f7a6d9a522de7dbeb6
Jason Pettrone <jasonpettrone@gmail.com>
2021-10-16 14:14:10 -0400

    Fixed typo in Introduction column in db

---
+++ API/Data/Migrations/20211016180325_FixIntroductionTypo.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class FixIntroductionTypo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Introudction",
+                table: "Users",
+                newName: "Introduction");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Introduction",
+                table: "Users",
+                newName: "Introudction");
+        }
+    }
+}
